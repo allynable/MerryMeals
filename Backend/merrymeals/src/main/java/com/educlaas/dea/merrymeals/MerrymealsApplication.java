76d0fa20f0db9d5b2f4ff8da956feadd30abe9c7
icable24 <114415997+allynable@users.noreply.github.com>
2023-05-18 09:41:16 +0800

    initial commit

---
+++ Backend/merrymeals/src/main/java/com/educlaas/dea/merrymeals/MerrymealsApplication.java
@@ -0,0 +1,13 @@
+package com.educlaas.dea.merrymeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerrymealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerrymealsApplication.class, args);
+	}
+
+}
