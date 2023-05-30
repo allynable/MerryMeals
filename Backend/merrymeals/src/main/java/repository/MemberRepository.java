d52ec6db776a6b53573e3bec6152919f749f045f
engr-topper <buentipochristopher@gmail.com>
2023-05-30 18:23:51 +0800

    member and partner repo

---
+++ Backend/merrymeals/src/main/java/repository/MemberRepository.java
@@ -0,0 +1,21 @@
+package com.mow.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mow.dao.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	//Check or Find existing email - return user which relates with existing email
+	//For Login 
+    Optional<Member> findByEmail(String email);
+
+    //Check or Find existing mail - return true or false
+    //For Register (duplicate mail)
+    Boolean existsByEmail(String email);
+
+}
