fa1832dca2e60b475a100c284217af47e33667b5
icable24 <114415997+allynable@users.noreply.github.com>
2023-05-31 20:23:07 +0800

    deleted main

--- Backend/merrymeals/src/main/java/com/educlaas/dea/merrymeals/repository/VolunteerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.educlaas.dea.merrymeals.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.educlaas.dea.merrymeals.dao.Volunteer;
-
-public interface VolunteerRepository extends JpaRepository<Volunteer, Long>{
-    
-}
