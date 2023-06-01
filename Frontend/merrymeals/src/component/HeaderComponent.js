229d542c93391d22adcf12c29450e72f550be17f
icable24 <114415997+allynable@users.noreply.github.com>
2023-06-01 23:12:46 +0800

    registration

--- Frontend/merrymeals/src/component/HeaderComponent.js
+++ Frontend/merrymeals/src/component/HeaderComponent.js
@@ -1,48 +1,67 @@
-import React, { Component } from 'react'
-import '../css/HeaderComponent.css'
-import logo from '../image/logo.jpg'
-
+import React, { Component } from "react";
+import "../css/HeaderComponent.css";
+import logo from "../image/logo.jpg";
 
 export class HeaderComponent extends Component {
   render() {
     return (
       <div>
-  <nav class="navbar">
-    <div class="navbar-logo">
-      <img src={logo} alt="Logo"/>
-    </div>
-    <div class="navbar-items">
-      <ul class="navbar-menu">
-        <li><a href="/" class="navbar-link">Home</a></li>
-        <li><a href="..." class="navbar-link">Meals</a></li>
-        <li><a href="/donate" class="navbar-link">Donate</a></li>
-        <li class="navbar-dropdown">
-          <a href="..." class="navbar-link">Register</a>
-          <div class="navbar-dropdown-content">
-            <a href="/memregistration">Member/Caregivers</a>
-            <a href="...">Member/Caregivers</a>
-            <br></br>
-            <a href="/pvregistration">Partner/Volunteer</a>
+        <nav className="navbar">
+          <div className="navbar-logo">
+            <img src={logo} alt="Logo" />
           </div>
-        </li>
-        <li class="navbar-dropdown">
-          <a href="..." class="navbar-link">Login</a>
-          <div class="navbar-dropdown-content">
-            <a href="...">Member/Caregivers</a>
-            <br></br>
-            <a href="/pvlogin">Partner/Volunteer</a>
+          <div className="navbar-items">
+            <ul className="navbar-menu">
+              <li>
+                <a href="/" className="navbar-link">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="..." className="navbar-link">
+                  Meals
+                </a>
+              </li>
+              <li>
+                <a href="/donate" className="navbar-link">
+                  Donate
+                </a>
+              </li>
+              <li className="navbar-dropdown">
+                <a href="..." className="navbar-link">
+                  Register
+                </a>
+                <div className="navbar-dropdown-content">
+                  <a href="/memregistration">Member/Caregivers</a>
+                  <a href="...">Member/Caregivers</a>
+                  <br></br>
+                  <a href="/pvregistration">Partner/Volunteer</a>
+                </div>
+              </li>
+              <li className="navbar-dropdown">
+                <a href="..." className="navbar-link">
+                  Login
+                </a>
+                <div className="navbar-dropdown-content">
+                  <a href="...">Member/Caregivers</a>
+                  <br></br>
+                  <a href="/pvlogin">Partner/Volunteer</a>
+                </div>
+              </li>
+            </ul>
+            <div className="navbar-search">
+              <input
+                type="text"
+                placeholder="Search"
+                className="navbar-search-input"
+              />
+              <button className="navbar-search-button">Search</button>
+            </div>
           </div>
-        </li>
-      </ul>
-      <div class="navbar-search">
-        <input type="text" placeholder="Search" class="navbar-search-input"/>
-        <button class="navbar-search-button">Search</button>
+        </nav>
       </div>
-    </div>
-  </nav>
-      </div>
-    )
+    );
   }
 }
 
-export default HeaderComponent
+export default HeaderComponent;
--- Frontend/merrymeals/src/component/MemberSignup.js
+++ Frontend/merrymeals/src/component/MemberSignup.js
@@ -1,91 +1,186 @@
-import React, { useState } from 'react';
-import {Link} from "react-router-dom";
-import '../css/MembersSignup.css';
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Container, Form, Row, Col, Button, Card } from "react-bootstrap";
+import "../css/MembersSignup.css";
 const RegistrationForm = () => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [address, setAddress] = useState('');
-    const [email, setEmail] = useState('');
-    const [contactNumber, setContactNumber] = useState('');
-    const [dob, setDob] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [condition, setCondition] = useState('');
-    const [allergies, setAllergies] = useState('');
-    const [caregiverName, setCaregiverName] = useState('');
-    const [relationship, setRelationship] = useState('');
-    const [caregiverContact, setCaregiverContact] = useState('');
-  
-    const handleRegistration = (e) => {
-      e.preventDefault();
-      // Handle form submission logic here
-    };
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+  const [dob, setDob] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [condition, setCondition] = useState("");
+  const [allergies, setAllergies] = useState("");
+  const [caregiverName, setCaregiverName] = useState("");
+  const [relationship, setRelationship] = useState("");
+  const [caregiverContact, setCaregiverContact] = useState("");
 
-    return (
-      <div id="form" className='container'>  
-        <form onSubmit={handleRegistration}>
-          <h1>Member Registration</h1>
-          <div>
-            <label htmlFor="firstName">First Name:</label>
-            <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
-          </div>
-          <div>
-            <label htmlFor="lastName">Last Name:</label>
-            <input type="text" id="lastName" name="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
-          </div>
-          <div>
-            <label htmlFor="address">Address:</label>
-            <input type="text-area" id="address" value={address} onChange={(e) => setAddress(e.target.value)} required/>
-          </div>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-          </div>
-          <div>
-            <label htmlFor="contact">Phone:</label>
-            <input type="tel" id="contactNumber" value={contactNumber} onChange={(e) => setContactNumber(e.target.value)} required />
-          </div>
-          <div>
-            <label htmlFor="dob">Birth Date:</label>
-            <input type="date" id="dob" value={dob} onChange={(e) => setDob(e.target.value)} required />
-          </div>
-          <div>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-          </div>
-          <div>
-            <label htmlFor="confirmPassword">Confirm Password:</label>
-            <input type="password" id="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
-          </div>
-          <div>
-            <label htmlFor="condition">Recent Illness or Accident</label>
-            <input type="text" id="condition" value={condition} onChange={(e) => setCondition(e.target.checked)} required/>
-          </div>
-          <div>
-            <label htmlFor="allergies">Food Allergies</label>
-            <input type="text" id="allergies" value={allergies} onChange={(e) => setAllergies(e.target.checked)} />
-          </div>
-          <div>
-            <label htmlFor="caregiverName">Caregiver's Name</label>
-            <input type="text" id="caregiverName" value={caregiverName} onChange={(e) => setCaregiverName(e.target.checked)} />
-          </div>
-          <div>
-            <label htmlFor="relationship">Relation</label>
-            <input type="text" id="relationship" value={relationship} onChange={(e) => setRelationship(e.target.checked)} />
-          </div>
-          <div>
-            <label htmlFor="caregiverContact">Relation</label>
-            <input type="tel" id="caregiverContact" value={caregiverContact} onChange={(e) => setCaregiverContact(e.target.checked)} />
-          </div>
-          <div>
-            <button type="submit">Register</button>
-          </div>
-          <span>Already have an account? <Link to="/Login">Login here</Link>.</span>
-        </form>
-      </div>  
-      );
-    };
-    
-    export default RegistrationForm;
-    
+  const handleRegistration = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  };
+
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Col  xs={12} sm={10} md={8}>
+          <Card className="shadow registration-card">
+            <Card.Body>
+              <h1>Member Registration</h1>
+              <br />
+              <Form onSubmit={handleRegistration}>
+                <Row>
+                  <Col>
+                    <Form.Group controlId="firstName">
+                      <Form.Control
+                        type="text"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        required
+                        placeholder="First Name"
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group controlId="lastName">
+                      <Form.Control
+                        type="text"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        required
+                        placeholder="Last Name"
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <br />
+                <Row>
+                <Form.Group controlId="address">
+                  <Form.Control
+                    as="textarea"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    required
+                    placeholder="Enter Address"
+                  />
+                </Form.Group>
+                </Row>
+                <Row>
+                  <Col>
+                  <Form.Group controlId="email">
+                  <Form.Control
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    placeholder="Email"
+                  />
+                </Form.Group>
+                  </Col>
+                  <Col>
+                  <Form.Group controlId="contactNumber">
+                  <Form.Control
+                    type="tel"
+                    value={contactNumber}
+                    onChange={(e) => setContactNumber(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+                  </Col>
+                </Row>
+
+                <Form.Group controlId="dob">
+                  <Form.Label>Birth Date:</Form.Label>
+                  <Form.Control
+                    type="date"
+                    value={dob}
+                    onChange={(e) => setDob(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="password">
+                  <Form.Label>Password:</Form.Label>
+                  <Form.Control
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="confirmPassword">
+                  <Form.Label>Confirm Password:</Form.Label>
+                  <Form.Control
+                    type="password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="condition">
+                  <Form.Label>Recent Illness or Accident:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={condition}
+                    onChange={(e) => setCondition(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="allergies">
+                  <Form.Label>Food Allergies:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={allergies}
+                    onChange={(e) => setAllergies(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="caregiverName">
+                  <Form.Label>Caregiver's Name:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={caregiverName}
+                    onChange={(e) => setCaregiverName(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="relationship">
+                  <Form.Label>Relation:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={relationship}
+                    onChange={(e) => setRelationship(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="caregiverContact">
+                  <Form.Label>Caregiver's Contact:</Form.Label>
+                  <Form.Control
+                    type="tel"
+                    value={caregiverContact}
+                    onChange={(e) => setCaregiverContact(e.target.value)}
+                  />
+                </Form.Group>
+                <br />
+                <Button variant="primary" type="submit">
+                  Register
+                </Button>
+                <br />
+                <Form.Text>
+                  Already have an account? <Link to="/Login">Login here</Link>.
+                </Form.Text>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default RegistrationForm;
