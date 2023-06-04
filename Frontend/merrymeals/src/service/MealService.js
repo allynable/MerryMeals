7d2b65223b54913aa5ee53625a3982b907f583b4
viclok60 <viclok118@gmail.com>
2023-06-04 18:49:43 +0800

    update

--- Frontend/merrymeals/src/service/MealService.js
+++ Frontend/merrymeals/src/service/MealService.js
@@ -4,6 +4,16 @@ class MealService{
     getMealItems() {
         return axios.get(API_BASE_URL + "meal/all");
     }
+    updateMeal(meal){
+        return axios.put(API_BASE_URL + "meal/" + meal.mealItemId, meal);
+    }
+    deleteMeal(mealItemId){
+        return axios.delete(API_BASE_URL + "meal/" + mealItemId);
+    }
+
+    saveMeal(meal){
+        return axios.post(API_BASE_URL + "savemeal", meal);
+    }
 }
 
 const mealService = new MealService()
