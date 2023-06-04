c746643790784724f881d0914d63770794b86481
icable24 <114415997+allynable@users.noreply.github.com>
2023-06-04 10:51:37 +0800

    added meal menu

--- Frontend/merrymeals/src/component/MenuComponent.js
+++ Frontend/merrymeals/src/component/MenuComponent.js
@@ -5,6 +5,9 @@ const MenuComponent = () => {
   const [mealItem, setMealItem] = useState([]);
   const mondayMenuItems = mealItem.filter((item) => item.day === "Monday");
   const tuesdayMenuItems = mealItem.filter((item) => item.day === "Tuesday");
+  const wednesdayMenuItems = mealItem.filter((item) => item.day === "Wednesday");
+  const thursdayMenuItems = mealItem.filter((item) => item.day === "Thursday");
+  const fridayMenuItems = mealItem.filter((item) => item.day === "Friday");
   const storeData = () => {
     mealService.getMealItems().then((response) => {
       setMealItem(response.data);
@@ -36,6 +39,39 @@ const MenuComponent = () => {
                 <h4>Tuesday</h4>
               </td>
               {mondayMenuItems.map((item) => (
+              <td>
+                <h5>{item.name}</h5>
+                <p>{item.description}</p>
+              </td>
+            ))}
+            </tr>
+            <tr>
+              <td>
+                <h4>Wednesday</h4>
+              </td>
+              {wednesdayMenuItems.map((item) => (
+              <td>
+                <h5>{item.name}</h5>
+                <p>{item.description}</p>
+              </td>
+            ))}
+            </tr>
+            <tr>
+              <td>
+                <h4>Thursday</h4>
+              </td>
+              {thursdayMenuItems.map((item) => (
+              <td>
+                <h5>{item.name}</h5>
+                <p>{item.description}</p>
+              </td>
+            ))}
+            </tr>
+            <tr>
+              <td>
+                <h4>Friday</h4>
+              </td>
+              {fridayMenuItems.map((item) => (
               <td>
                 <h5>{item.name}</h5>
                 <p>{item.description}</p>
